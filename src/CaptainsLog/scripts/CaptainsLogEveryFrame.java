36b6627f5768ba3fa979c8982d0cbc4bd02e0208
stormbringer951 <stormbringer951@gmail.com>
2021-03-30 10:12:04 +0100

    Refactoring

--- src/CaptainsLog/scripts/CaptainsLogEveryFrame.java
+++ src/CaptainsLog/scripts/CaptainsLogEveryFrame.java
@@ -1,6 +1,5 @@
 package CaptainsLog.scripts;
 
-import CaptainsLog.campaign.intel.CustomMessageIntel;
 import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.PlanetAPI;
@@ -22,7 +21,6 @@ import CaptainsLog.campaign.intel.RuinsIntelv2;
 import CaptainsLog.campaign.intel.SalvageableIntel;
 import CaptainsLog.campaign.intel.UnremovableIntel;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 public class CaptainsLogEveryFrame implements EveryFrameScript {
