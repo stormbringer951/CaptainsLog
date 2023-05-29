47b55938039bd8a6044043685f337cc8530b2498
stormbringer951 <31985582+stormbringer951@users.noreply.github.com>
2023-05-30 00:31:29 +0100

    Add comments and documentation

--- src/CaptainsLog/campaign/intel/button/LayInCourse.java
+++ src/CaptainsLog/campaign/intel/button/LayInCourse.java
@@ -43,6 +43,8 @@ public class LayInCourse implements IntelButton {
 
     @Override
     public boolean shouldCreateButton() {
+        // do not allow users to lay in course to entities that are expired/not clickable
+        // prevents user from repeatedly salvaging already salvaged content
         return !target.hasTag(Tags.FADING_OUT_AND_EXPIRING) && target.hasTag(Tags.NON_CLICKABLE);
     }
 }
