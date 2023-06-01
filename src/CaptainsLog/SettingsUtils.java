98cf4f1ad88d5d20d8099f94d79f980aa30c5b02
stormbringer951 <31985582+stormbringer951@users.noreply.github.com>
2023-06-01 20:51:46 +0100

    Add LunaLib toggle for custom intel importance (#75)

--- src/CaptainsLog/SettingsUtils.java
+++ src/CaptainsLog/SettingsUtils.java
@@ -6,30 +6,34 @@ import lunalib.lunaSettings.LunaSettings;
 public final class SettingsUtils {
 
     public static boolean excludeCommRelays() {
-        return !getBooleanSetting("CaptainsLog_Enable_Comm_Relays");
+        return !getBooleanSetting("CaptainsLog_Enable_Comm_Relays", true);
     }
 
     public static boolean excludeMegastructures() {
-        return !getBooleanSetting("CaptainsLog_Enable_Colony_Structures");
+        return !getBooleanSetting("CaptainsLog_Enable_Colony_Structures", true);
     }
 
     public static boolean excludeRuinsReports() {
-        return !getBooleanSetting("CaptainsLog_Enable_Ruins");
+        return !getBooleanSetting("CaptainsLog_Enable_Ruins", true);
     }
 
     public static boolean excludeSalvageableReports() {
-        return !getBooleanSetting("CaptainsLog_Enable_Salvageable");
+        return !getBooleanSetting("CaptainsLog_Enable_Salvageable", true);
     }
 
-    private static boolean getBooleanSetting(String s) {
+    private static boolean getBooleanSetting(String s, boolean defaultValue) {
         Boolean setting = LunaSettings.getBoolean(Constants.MOD_ID, s);
         if (setting == null) {
-            return false; // default faulse
+            return defaultValue;
         }
         return setting;
     }
 
     public static boolean isStelnetEnabled() {
         return Global.getSettings().getModManager().isModEnabled("stelnet");
+    }
+
+    public static boolean markCustomMessagesAsImportant() {
+        return getBooleanSetting("CaptainsLog_Mark_Custom_Messages_Important", true);
     }
 }
